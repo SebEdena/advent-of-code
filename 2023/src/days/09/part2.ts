61302203f98d69d17a177d7031d940477e0a07b1
Sebastien Viguier <viguier.sebastien@orange.fr>
2023-12-09 10:35:07 +0100

    day 09 2023

---
+++ 2023/src/days/09/part2.ts
@@ -0,0 +1,29 @@
+import { readInput } from '../../utils/readfile';
+
+const data = await readInput(import.meta.resolveSync('./input.txt'));
+
+const valuesList = data.map((row) => row.map((num) => parseInt(num)));
+
+const nextValues = [];
+
+for (const values of valuesList) {
+  const rows = [[...values]];
+  let currentRow = rows[0];
+  while (currentRow.some((num) => num !== 0)) {
+    const newRow = [];
+    for (let i = 0; i < currentRow.length - 1; i++) {
+      newRow[i] = currentRow[i + 1] - currentRow[i];
+    }
+    rows.push(newRow);
+    currentRow = newRow;
+  }
+  let startNum = 0;
+  for (let i = rows.length - 2; i >= 0; i--) {
+    startNum = rows[i][0] - startNum;
+  }
+  nextValues.push(startNum);
+}
+
+const sum = nextValues.reduce((sum, num) => sum + num, 0);
+
+console.log(sum);
