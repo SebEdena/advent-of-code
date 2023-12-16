e7a728b68b572149023705c2a66cde30271159a0
Sebastien Viguier <viguier.sebastien@orange.fr>
2023-12-16 15:03:55 +0100

    update readfile method

--- 2023/src/utils/readfile.ts
+++ 2023/src/utils/readfile.ts
@@ -1,11 +1,21 @@
 import { detectNewlineGraceful } from 'detect-newline';
 
-export async function readInput(filename: string) {
+const DEFAULT_OPTIONS: ReadOptions = {
+  keepEmptyLines: false,
+};
+
+export type ReadOptions = {
+  keepEmptyLines: boolean;
+};
+
+export async function readInput(filename: string, options: Partial<ReadOptions> = DEFAULT_OPTIONS) {
+  const { keepEmptyLines } = { ...DEFAULT_OPTIONS, ...options };
   const text = await Bun.file(filename).text();
+
   if (text.length > 0) {
     return text
       .split(detectNewlineGraceful(text))
-      .filter((row) => row.length > 0)
+      .filter((row) => (keepEmptyLines ? true : row.length > 0))
       .map((row) => row.split(/[ ]+/));
   } else {
     return [];
