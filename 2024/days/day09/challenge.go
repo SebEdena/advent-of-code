ff901e6793fb3a616ab6851cff9d8041abaa102f
Sebastien Viguier <viguier.sebastien@orange.fr>
2024-12-09 17:09:56 +0100

    day 9 part 1

---
+++ 2024/days/day09/challenge.go
@@ -0,0 +1,169 @@
+package main
+
+import (
+	"aoc-2024/util/io"
+	"flag"
+	"fmt"
+	"slices"
+	"strconv"
+)
+
+func main() {
+	input := io.ReadInputFile("./input.txt")
+
+	var part int
+	flag.IntVar(&part, "part", 1, "part 1 or 2")
+	flag.Parse()
+
+	var result int
+	if part == 1 {
+		result = part1(input)
+	} else if part == 2 {
+		result = part2(input)
+	} else {
+		panic("Invalid part")
+	}
+
+	fmt.Println(result)
+}
+
+func part1(input *string) int {
+	// data := io.ToStringSlice(input, " ")
+	fileBlocks, freeBlocks, readerIndex := parseInput(input)
+
+mainLoop:
+	for len(freeBlocks) > 0 {
+		var fileBlock *FileBlock
+		fileBlock, readerIndex = getBlockAtIndex(fileBlocks, readerIndex)
+
+		for len(freeBlocks) > 0 && fileBlock != nil {
+			freeBlock := &freeBlocks[0]
+
+			if freeBlock.start > readerIndex {
+				break mainLoop
+			}
+
+			if fileBlock.length < freeBlock.length {
+				moveBlock := FileBlock{
+					id: fileBlock.id,
+					Block: Block{
+						start:  freeBlock.start,
+						length: fileBlock.length,
+					},
+				}
+
+				freeBlock.length -= fileBlock.length
+				freeBlock.start += fileBlock.length
+
+				delete(fileBlocks, readerIndex)
+				fileBlocks[moveBlock.start] = moveBlock
+				fileBlock = nil
+			} else {
+				fileBlocks[freeBlock.start] = FileBlock{
+					id: fileBlock.id,
+					Block: Block{
+						start:  freeBlock.start,
+						length: freeBlock.length,
+					},
+				}
+
+				fileBlock = &FileBlock{
+					id: fileBlock.id,
+					Block: Block{
+						start:  fileBlock.start,
+						length: fileBlock.length - freeBlock.length,
+					},
+				}
+
+				fileBlocks[fileBlock.start] = *fileBlock
+				freeBlocks = slices.Delete(freeBlocks, 0, 1)
+
+				if fileBlock.length == 0 {
+					delete(fileBlocks, readerIndex)
+					break
+				}
+			}
+		}
+		readerIndex--
+	}
+
+	checksum := 0
+	for _, block := range fileBlocks {
+		for i := 0; i < block.length; i++ {
+			checksum += (block.start + i) * block.id
+		}
+	}
+
+	return checksum
+}
+
+func part2(input *string) int {
+	// data := io.ToStringSlice(input, " ")
+
+	return 0
+}
+
+type Block struct {
+	start  int
+	length int
+}
+
+type FileBlock struct {
+	id int
+	Block
+}
+
+func parseInput(input *string) (map[int]FileBlock, []Block, int) {
+	fileBlocks := make(map[int]FileBlock)
+	freeBlocks := make([]Block, 0)
+	id := 0
+	index := 0
+	isEmpty := false
+
+	for i := 0; i < len(*input); i++ {
+		num, err := strconv.Atoi(string((*input)[i]))
+
+		if err != nil {
+			panic(err)
+		}
+
+		if isEmpty {
+			if num != 0 {
+				freeBlocks = append(freeBlocks, Block{
+					start:  index,
+					length: num,
+				})
+			}
+		} else {
+			fileBlocks[index] = FileBlock{
+				id: id,
+				Block: Block{
+					start:  index,
+					length: num,
+				},
+			}
+			id++
+		}
+
+		if i < len(*input)-1 {
+			index += num
+			isEmpty = !isEmpty
+		}
+	}
+	return fileBlocks, freeBlocks, index
+}
+
+func getBlockAtIndex(blocks map[int]FileBlock, index int) (*FileBlock, int) {
+	startIndex := index
+
+	for blocks[startIndex].length == 0 {
+		if startIndex < 0 {
+			return nil, -1
+		}
+		startIndex--
+	}
+
+	block := blocks[startIndex]
+
+	return &block, startIndex
+}
